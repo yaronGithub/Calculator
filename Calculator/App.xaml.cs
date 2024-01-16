158540b82edab047a3e2b2df9e649954867bb06d
yaronGithub <yaron.traitel@gmail.com>
2024-01-16 14:37:45 +0200

    ok

--- Calculator/App.xaml.cs
+++ Calculator/App.xaml.cs
@@ -6,7 +6,7 @@
         {
             InitializeComponent();
 
-            MainPage = new Ex();
+            MainPage = new MainPage();
         }
     }
 }
--- Calculator/MainPage.xaml.cs
+++ Calculator/MainPage.xaml.cs
@@ -1,10 +1,44 @@
-﻿namespace Calculator
+﻿using System.ComponentModel;
+
+namespace Calculator
 {
+    public class CalculatorBrain : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+        public CalculatorBrain() 
+        {
+            ButtonCommand = new Command(() => ChangeOutput(output));
+        }
+        private string output;
+        public string Output
+        {
+            get { return output; }
+            set { output = value; OnPropertyChanged(); }
+        }
+        public Command ButtonCommand { get; set; }
+        private void ChangeOutput(string str)
+        {
+            this.Output += str;
+            ButtonCommand.ChangeCanExecute();
+        }
+    }
     public partial class MainPage : ContentPage
     {
+        CalculatorBrain brain;
         public MainPage()
         {
             InitializeComponent();
+            brain = new CalculatorBrain();
+            BindingContext = brain;
         }
     }
 }
